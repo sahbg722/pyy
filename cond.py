ac5d3f9d72494504c94c8d3c31f802e2a3724121
sahbg722 <144184809+sahbg722@users.noreply.github.com>
2023-10-05 21:42:40 -0300

    Add files via upload

---
+++ cond.py
@@ -0,0 +1,40 @@
+#composta 1
+# nota = int(input('digite a nota: '))
+
+# if nota == 10:
+   # print('excelente!')
+# elif nota <= 5:
+    #print ('melhore!')
+# else:
+    #print('bom')
+
+#composta 2
+
+# idade = int(input('digite a sua idade: '))
+# sexo =input('digite o sexo m ou f: ').lower()
+
+# if idade < 18 and sexo == 'm':
+#    print('homem menor de idade')
+#elif idade >= 18 and sexo == 'm':
+ #   print('homem maior de idade')
+#elif idade < 18 and sexo == 'f':
+#    print('mulher menor de idade')
+#elif idade >= 18 and sexo == 'f':
+#    print('mulher maior de idade')
+
+#else:
+ #   print('erro na entrada de dados')
+
+#numero = int(input('digite um numero: '))
+
+#if numero %2 == 0:
+#    print('o numero e par. ')
+#else:
+ #   print('o numero e impar. ')
+
+numero = int(input('digite um numero: '))
+
+if numero %2 == 0:
+    print('o numero e par. ')
+else:
+    print('o numero e impar. ')
---
+++ lacoderep.py
@@ -0,0 +1,33 @@
+#x = 0
+
+#while x < 5:
+ #   nome = input('qual e o seu nome? ')
+#   x = x+1
+
+#atividade 2
+
+#lista_compras = []
+#quantidade = 0
+
+#while quantidade < 5:
+#    itens_lista = input('qual item voce quer adicionar a lista?' )
+#    lista_compras.append (itens_lista)
+ #   quantidade = quantidade +1
+
+#print('a minha lista dee compras finlal: ', lista_compras)
+
+#atividade 3
+
+#faturamento = [100, 250, 220, 500, 2000]
+#total = 0
+
+#for i in faturamento:
+#    total =total +1
+    
+#print(total)
+
+#atividade 4
+
+for i in range (0,11):
+ #   neste looping mostrara os numeros ordenados de 0 ate 10
+    print(i)
